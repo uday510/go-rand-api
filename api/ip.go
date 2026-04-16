ac4ccc944b9f3011c26b848b7b2fced6120a8782
Uday Teja <62304758+uday510@users.noreply.github.com>
2026-04-16 18:18:32 +0530

    update ip fn '

--- api/ip.go
+++ api/ip.go
@@ -3,30 +3,31 @@ package handler
 import (
 	"net"
 	"net/http"
+	"strings"
 )
 
 func getIP(r *http.Request) string {
-	ip := r.Header.Get("x-forwarded-for")
+	ip := r.Header.Get("CF-Connecting-IP")
 	if ip != "" {
 		return ip
 	}
 
-	ip = r.Header.Get("x-real-ip")
+	ip = r.Header.Get("X-Forwarded-For")
+	if ip != "" {
+		parts := strings.Split(ip, ",")
+		return strings.TrimSpace(parts[0])
+	}
+
+	ip = r.Header.Get("X-Real-IP")
 	if ip != "" {
 		return ip
 	}
 
+	// 4. RemoteAddr fallback
 	host, _, err := net.SplitHostPort(r.RemoteAddr)
 	if err != nil {
 		return r.RemoteAddr
 	}
 
 	return host
-}
-
-func IPHandler(w http.ResponseWriter, r *http.Request) {
-	ip := getIP(r)
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(ip + "\n"))
 }
