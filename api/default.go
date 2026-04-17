351ac171d3dc48b1260d4cf29db0571dbbf2625c
Uday Teja <62304758+uday510@users.noreply.github.com>
2026-04-17 10:53:54 +0530

    update default route

--- api/default.go
+++ api/default.go
@@ -1,9 +1,10 @@
 package handler
 
-import "net/http"
+import (
+	"apis/lib"
+	"net/http"
+)
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("welcome to earth, human. you seem lost.\n"))
+	lib.WriteTextResponse(w, r, "welcome to earth, human. you seem lost.")
 }
