cf00ea6fa0cb3a2babc5ab26811da6dbd1a032f7
Uday Teja <62304758+uday510@users.noreply.github.com>
2026-04-17 10:50:05 +0530

    update code

---
+++ lib/response.go
@@ -0,0 +1,21 @@
+package lib
+
+import "net/http"
+
+func WriteTextResponse(w http.ResponseWriter, r *http.Request, body string) {
+	reqID := FromRequest(r)
+	w.Header().Set("X-Request-Id", reqID)
+
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	w.Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate")
+	w.Header().Set("Pragma", "no-cache")
+	w.Header().Set("Expires", "0")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+
+	if cfRay := r.Header.Get("CF-Ray"); cfRay != "" {
+		w.Header().Set("CF-Ray", cfRay)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(body + "\n"))
+}
